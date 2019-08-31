d736e9b063508d1e46cd556dead631af03a102f7
huangys <770302485@qq.com>
2019-08-31 21:42:55 +0800

    submit goods components

---
+++ src/components/cart-control/cart-control.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'cart-control'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'ratings'
+  }
+</script>
+
+<style scoped>
+
+</style>
